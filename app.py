2cc65c11dba77353f995f4be46784bb55a2556dc
Shiyang <39637745+EisenShiyang@users.noreply.github.com>
2023-01-17 20:37:09 +0800

    Removed deprecated modules

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
-
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-#======這裡是呼叫的檔案內容=====
-
 #======python的函數庫==========
 import tempfile, os
 import datetime
