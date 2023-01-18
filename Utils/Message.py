ca768db3bc5f14e1dbf2a339efa503033ecce33c
Shiyang <39637745+EisenShiyang@users.noreply.github.com>
2023-01-18 11:52:29 +0800

    Create Utils folder

---
+++ Utils/Message.py
@@ -0,0 +1,6 @@
+HELP = """🥸"""
+CODE_INFO = """GitHub : https://github.com/EisenShiyang/hwaya_bot
+Technologies used : Python, MongoDB, Fly.io
+You are welcome to fork the project to add in any feature or optimize the code🤠"""
+COMMAND_NOT_FOUND = """🤖: 尚未支援此指令ㄛ🥴
+        請輸入 /help 查詢支援指令"""
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,12 +6,11 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
-#======python的函數庫==========
+import os
 import tempfile, os
 import datetime
 import time
-#======python的函數庫==========
+from Utils import Constant, Message
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
@@ -22,7 +20,7 @@ line_bot_api = LineBotApi('2ntK2P9daJrdC8r0dyZ6AVe5jDfGIdf27wOWkn1pZ/VZEMOOjKhth
 handler = WebhookHandler('a7142968d06edae024e33e9f592d5413')
 
 
-# 監聽所有來自 /callback 的 Post Request
+# Listen all requests from /callback
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -37,19 +35,23 @@ def callback():
         abort(400)
     return 'OK'
 
-
-# 處理訊息
+# Main method for handling message
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-
-    # JUST FOR TESTING
-    msg = "EEEEEEE"
+    
+    if msg in Constant.HELP:
+        msg = Message.HELP
+    elif msg in Constant.CODE:
+        msg = Message.CODE_INFO
+    elif msg in Constant.FOOD:
+        msg = "想存東西R人類"
+    else:
+        msg = Message.COMMAND_NOT_FOUND
 
     message = TextSendMessage(text=msg)
     line_bot_api.reply_message(event.reply_token, message)
         
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
